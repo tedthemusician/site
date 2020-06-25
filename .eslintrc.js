6679531adb53c4c7220a9cffd71a00889aa4ad33
Ted Oliver <ted@teds-mbp.attlocal.net>
2020-06-24 21:41:25 -0400

    Disallow semicolons, require trailing commas

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,16 @@ module.exports = {
         'eslint:recommended',
     ],
     rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-console': process.env.NODE_ENV === 'production'
+            ? 'error'
+            : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production'
+            ? 'error'
+            : 'off',
+        semi: process.env.NODE_ENV === 'production'
+            ? 'off'
+            : [2, 'never'],
+        'comma-dangle': ['error', 'always-multiline'],
     },
     parserOptions: {
         parser: 'babel-eslint',
