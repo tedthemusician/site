998bef743fe61996820917f63ee7b7c3826284c5
Ted Oliver <ted@teds-mbp.attlocal.net>
2020-06-25 22:17:18 -0400

    Add development showcase view

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/views/Home.vue'
 import MusicShowcase from '@/views/MusicShowcase.vue'
+import DevelopmentShowcase from '@/views/DevelopmentShowcase.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
         path: '/musician',
         name: 'Musician',
         component: MusicShowcase,
+    },
+    {
+        path: '/developer',
+        name: 'Developer',
+        component: DevelopmentShowcase,
     },
 ]
 
---
+++ src/views/DevelopmentShowcase.vue
@@ -0,0 +1,15 @@
+<template>
+    <main>
+        This is the development showcase
+    </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
