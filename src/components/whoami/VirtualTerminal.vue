5a07169616777a957194ade643bc3b1f182ae808
Ted Oliver <ted@teds-mbp.attlocal.net>
2020-07-04 14:13:01 -0400

    Add placeholder termianl content and initial dynamic styles

--- src/components/whoami/VirtualTerminal.vue
+++ src/components/whoami/VirtualTerminal.vue
@@ -1,22 +1,29 @@
 <template>
-<main>
-    <div id="title-bar"></div>
-    <div id="terminal"></div>
+<main :style="windowStyle">
+    <div id="title-bar" :style="titleBarStyle">
+        <span id="title">PreTYY</span>
+    </div>
+    <div id="content" :style="contentStyle">
+        This is the content of the window.
+    </div>
 </main>
 </template>
 
 <script>
 export default {
-
+    computed: {
+        windowStyle() {
+            return this.$store.getters.getStyle('whoami', 'window')
+        },
+        titleBarStyle() {
+            return this.$store.getters.getStyle('whoami', 'titleBar')
+        },
+        contentStyle() {
+            return this.$store.getters.getStyle('whoami', 'content')
+        },
+    },
 }
 </script>
 
 <style scoped>
-main {
-    min-width: 6rem;
-    min-height: 20rem;
-
-    background: black;
-
-}
 </style>
