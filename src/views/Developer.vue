7ac52b3e67618a0a2f4b62a82fe40301221b56db
Ted Oliver <ted@teds-mbp.attlocal.net>
2020-07-13 12:35:00 -0400

    Add list of projects to dev showcase

--- src/views/Developer.vue
+++ src/views/Developer.vue
@@ -1,12 +1,52 @@
 <template>
     <main>
-        This is the development showcase
+        <div v-for="project in projects" :key="project.id">
+            <a>
+                <h2 v-text="project.name" />
+            </a>
+            <p v-text="project.comment" />
+        </div>
     </main>
 </template>
 
 <script>
-export default {
+const projects = [
+    {
+        id: 'pangram',
+        name: 'Pangram finder',
+        comment: 'A tool for finding new pangrams',
+    },
+    {
+        id: 'dvd',
+        name: 'Evil DVD Screensaver',
+        comment: 'A DVD screensaver whose icon never quite hits the corner',
+    },
+    {
+        id: 'mystify',
+        name: 'Mystify Your Mind',
+        comment: 'A bit of Windows 95 nostalgia',
+    },
+    {
+        id: 'deepnote',
+        name: 'Deep Note',
+        comment: 'A visualization of a familiar sound',
+    },
+    {
+        id: 'life',
+        name: 'Life',
+        comment: "Conway's Game of Life",
+    },
+    {
+        id: 'julia',
+        name: 'Julia Sets',
+        comment: 'A Julia Set manipulator',
+    },
+]
 
+export default {
+    data: () => ({
+        projects,
+    }),
 }
 </script>
 
