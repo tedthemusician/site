0636a652459617110494978ba82bf0382f6673d6
Ted Oliver <ted@teds-mbp.attlocal.net>
2020-07-13 12:49:57 -0400

    Add initial developer styles object

--- src/styles/themes.js
+++ src/styles/themes.js
@@ -1,9 +1,11 @@
-import app from './app.js'
-import home from './home.js'
-import whoami from './whoami.js'
+import app       from './app.js'
+import developer from './developer.js'
+import home      from './home.js'
+import whoami    from './whoami.js'
 
 export default {
     app,
+    developer,
     home,
     whoami,
 }
