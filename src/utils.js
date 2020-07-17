68b3b4f04d8f929c9f146aec73eb9a99741691b7
Ted Oliver <ted@teds-mbp.attlocal.net>
2020-07-17 14:24:28 -0400

    Add randInt()

--- src/utils.js
+++ src/utils.js
@@ -3,6 +3,10 @@ export function randRange(low, high) {
     return (Math.random() * difference) + low
 }
 
+export function randInt(low, high) {
+    return Math.round(randRange(low, high))
+}
+
 export function pxToRem(px) {
     return px / parseFloat(getComputedStyle(document.documentElement).fontSize)
 }
